a6aae53f0aa0e680cd708ddfa657bb5b63daf606
justinHume <justin.df.hume@gmail.com>
2013-02-19 10:00:40 -0800

    Refactored the generator API to separate generation of a CFI into a package document AND content document into different methods

--- spec/javascripts/cfi_generation_spec.js
+++ spec/javascripts/cfi_generation_spec.js
@@ -112,7 +112,9 @@ describe("CFI GENERATOR", function () {
         var contentDoc = (new window.DOMParser).parseFromString(contentDocXhtml, "text/xml");
         var packageDoc = (new window.DOMParser).parseFromString(packageDocXhtml, "text/xml");
 
-        var generatedCFI = EPUBcfi.Generator.generateCharacterOffsetCFI($('#startParent', contentDoc).contents()[1], 3, "contentDocId", packageDoc);
+        var contentDocCFIComponent = EPUBcfi.Generator.generateCharacterOffsetCFIComponent($('#startParent', contentDoc).contents()[1], 3);
+        var packageDocCFIComponent = EPUBcfi.Generator.generatePackageDocumentCFIComponent("contentDocId", packageDoc);
+        var generatedCFI = EPUBcfi.Generator.generateCompleteCFI(packageDocCFIComponent, contentDocCFIComponent);
 
         expect(generatedCFI).toEqual("epubcfi(/6/2/6!/4/2[startParent]/3:3)"); // [ te,xtn]
     });
@@ -124,7 +126,9 @@ describe("CFI GENERATOR", function () {
         var packageDocXhtml = jasmine.getFixtures().read("moby_dick_package.opf");
         var packageDoc = (new window.DOMParser).parseFromString(packageDocXhtml, "text/xml");
 
-        var generatedCFI = EPUBcfi.Generator.generateCharacterOffsetCFI($("#c01p0008", contentDoc)[0].firstChild, 103, "xchapter_001", packageDoc);
+        var contentDocCFIComponent = EPUBcfi.Generator.generateCharacterOffsetCFIComponent($("#c01p0008", contentDoc)[0].firstChild, 103);
+        var packageDocCFIComponent = EPUBcfi.Generator.generatePackageDocumentCFIComponent("xchapter_001", packageDoc);
+        var generatedCFI = EPUBcfi.Generator.generateCompleteCFI(packageDocCFIComponent, contentDocCFIComponent);
 
         expect(generatedCFI).toEqual("epubcfi(/6/14!/4[body1]/2/18[c01p0008]/1:103)"); // [, a,lof]
     });
@@ -136,7 +140,9 @@ describe("CFI GENERATOR", function () {
         var packageDocXhtml = jasmine.getFixtures().read("moby_dick_package.opf");
         var packageDoc = (new window.DOMParser).parseFromString(packageDocXhtml, "text/xml");
 
-        var generatedCFI = EPUBcfi.Generator.generateElementCFI($("#c01p0008", contentDoc)[0], "xchapter_001", packageDoc);
+        var contentDocCFIComponent = EPUBcfi.Generator.generateElementCFIComponent($("#c01p0008", contentDoc)[0]);
+        var packageDocCFIComponent = EPUBcfi.Generator.generatePackageDocumentCFIComponent("xchapter_001", packageDoc);
+        var generatedCFI = EPUBcfi.Generator.generateCompleteCFI(packageDocCFIComponent, contentDocCFIComponent);
 
         expect(generatedCFI).toEqual("epubcfi(/6/14!/4[body1]/2/18[c01p0008])");
     });
@@ -148,7 +154,9 @@ describe("CFI GENERATOR", function () {
         var packageDocXhtml = jasmine.getFixtures().read("moby_dick_package.opf");
         var packageDoc = (new window.DOMParser).parseFromString(packageDocXhtml, "text/xml");
 
-        var generatedCFI = EPUBcfi.Generator.generateElementCFI($("html", contentDoc)[0], "xchapter_001", packageDoc);
+        var contentDocCFIComponent = EPUBcfi.Generator.generateElementCFIComponent($("html", contentDoc)[0]);
+        var packageDocCFIComponent = EPUBcfi.Generator.generatePackageDocumentCFIComponent("xchapter_001", packageDoc);
+        var generatedCFI = EPUBcfi.Generator.generateCompleteCFI(packageDocCFIComponent, contentDocCFIComponent);        
 
         expect(generatedCFI).toEqual("epubcfi(/6/14!/2)");
     });
@@ -174,7 +182,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if a text node is not supplied as a starting point", function () {
 
             expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(undefined, 103, "xchapter_001", packageDoc)})
+                EPUBcfi.Generator.generateCharacterOffsetCFIComponent(undefined, 103, "xchapter_001", packageDoc)})
             .toThrow(
                 EPUBcfi.NodeTypeError(undefined, "Cannot generate a character offset from a starting point that is not a text node")
             );
@@ -184,7 +192,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if the character offset is less then 0", function () {
 
            expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(startTextNode, -1, "xchapter_001", packageDoc)})
+                EPUBcfi.Generator.generateCharacterOffsetCFIComponent(startTextNode, -1, "xchapter_001", packageDoc)})
             .toThrow(
                 EPUBcfi.OutOfRangeError(-1, 0, "Character offset cannot be less than 0")
             ); 
@@ -194,7 +202,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if the character offset is greater than the length of the text node", function () {
 
            expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(startTextNode, startTextNode.nodeValue.length + 1, "xchapter_001", packageDoc)})
+                EPUBcfi.Generator.generateCharacterOffsetCFIComponent(startTextNode, startTextNode.nodeValue.length + 1, "xchapter_001", packageDoc)})
             .toThrow(
                 EPUBcfi.OutOfRangeError(
                     startTextNode.nodeValue.length + 1, 
@@ -207,7 +215,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if an idref is not supplied", function () {
 
             expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(startTextNode, 1, undefined, packageDoc)})
+                EPUBcfi.Generator.generatePackageDocumentCFIComponent(undefined, packageDoc)})
             .toThrow(
                 Error("The idref for the content document, as found in the spine, must be supplied")
             );
@@ -217,7 +225,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if a package document is not supplied", function () {
 
             expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(startTextNode, 1, "xchapter_001", undefined)})
+                EPUBcfi.Generator.generatePackageDocumentCFIComponent("xchapter_001", undefined)})
             .toThrow(
                 Error("A package document must be supplied to generate a CFI")
             );
@@ -227,7 +235,7 @@ describe("CFI GENERATOR", function () {
         it("throws an error if the idref does not match any idref attribute on itemref elements in the spine", function () {
 
             expect(function () {
-                EPUBcfi.Generator.generateCharacterOffsetCFI(startTextNode, 1, "xchapter_", packageDoc)})
+                EPUBcfi.Generator.generatePackageDocumentCFIComponent("xchapter_", packageDoc)})
             .toThrow(
                 Error("The idref of the content document could not be found in the spine")
             );
