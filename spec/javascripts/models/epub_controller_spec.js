c7986fec2088ad2b1bc6a77fe10eef7f757fbd3f
justinHume <justin.df.hume@gmail.com>
2013-03-19 10:48:40 -0700

    Removed the epub model, as it is now redundant

--- spec/javascripts/models/epub_controller_spec.js
+++ spec/javascripts/models/epub_controller_spec.js
@@ -2,25 +2,12 @@ describe("Epub.EPUBController", function() {
         
     describe("initialization", function() {
 
-        beforeEach(function() {
-            return stubFileSystem();
-        });
-
         describe("with valid params", function() {
 
             beforeEach(function() {
                 this.epub = new Epub.EPUB({
                     "package_doc_path": "some/file/path"
                 });
-            });
-
-            it("initializes a pagination strategy selector", function() {
-
-                var epub;
-                epub = new Epub.EPUBController({
-                    "epub": this.epub
-                });
-                expect(epub.paginator).toBeDefined();
             });
 
             it("initializes a reference to the package document", function() {
@@ -31,62 +18,9 @@ describe("Epub.EPUBController", function() {
                 });
                 expect(epubController.packageDocument).toBeDefined();
             });
-
-            it("calls fetch on the package document", function() {
-              
-                var epub;
-                var epubController;
-                var packageDocument;
-
-                packageDocument = Factory.spy("package_document");
-                spyOn(Epub, "PackageDocument").andReturn(packageDocument);            
-                epub = new Epub.EPUB({"package_doc_path": "some/file/path"});
-                epubController = new Epub.EPUBController({
-                    "epub": epub
-                });
-
-                expect(Epub.PackageDocument).toHaveBeenCalled();
-                expect(packageDocument.fetch).toHaveBeenCalled();
-            });
-
-            describe("sets up event handlers", function() {
-                
-                beforeEach(function() {
-                    this.epub = new Epub.EPUB({
-                        "package_doc_path": "some/file/path"
-                    });
-                    this.epubController = new Epub.EPUBController({
-                        "epub": this.epub
-                    });
-                });
-            });
         });
     });
 
-    describe("defaults", function() {
-
-        beforeEach(function() {
-            this.epub = new Epub.EPUB({
-                "package_doc_path": "some/file/path"
-            });
-            this.epubController = new Epub.EPUBController({
-                "epub": this.epub
-            });
-        });
-        
-        it('correctly sets default attributes', function() {
-
-            expect(this.epubController.get("font_size")).toEqual(10);
-            expect(this.epubController.get("two_up")).toEqual(false);
-            expect(this.epubController.get("full_screen")).toEqual(false);
-            expect(this.epubController.get("toolbar_visible")).toEqual(true);
-            expect(this.epubController.get("toc_visible")).toEqual(false);
-            expect(this.epubController.get("rendered_spine_items")).toEqual([]);
-            expect(this.epubController.get("current_theme")).toEqual("default-theme");
-            expect(this.epubController.get("current_margin")).toEqual(3);
-        });
-    });
-    
     describe("toJSON", function() {
 
         beforeEach(function() {
--- src/epub/models/epub_controller.js
+++ src/epub/models/epub_controller.js
@@ -18,7 +18,7 @@ Epub.EPUBController = Backbone.Model.extend({
 	//  "PUBLIC" METHODS (THE API)                                                          //
 	// ------------------------------------------------------------------------------------ //
 
-	initialize : function() {
+	initialize : function (attributes, options) {
 
 		// capture context for use in callback functions
 		var that = this;
@@ -85,6 +85,36 @@ Epub.EPUBController = Backbone.Model.extend({
 		}
 	},
 
+    // toJSON: function() {
+
+    //     // only save attrs that should be persisted:
+    //     return {
+    //         "apple_fixed": this.get("apple_fixed"),
+    //         "author": this.get("author"),
+    //         "cover_href": this.get("cover_href"),
+    //         "created_at": this.get("created_at"),
+    //         "description": this.get("description"),
+    //         "epub_version": this.get("epub_version"),
+    //         "fixed_layout": this.get("fixed_layout"),
+    //         "id": this.get("id"),
+    //         "key": this.get("key"),
+    //         "language": this.get("language"),
+    //         "layout": this.get("layout"),
+    //         "modified_date": this.get("modified_date"),
+    //         "ncx": this.get("ncx"),
+    //         "open_to_spread": this.get("open_to_spread"),
+    //         "orientation": this.get("orientation"),
+    //         "package_doc_path": this.get("package_doc_path"),
+    //         "page_prog_dir": this.get("page_prog_dir"),
+    //         "paginate_backwards": this.get("paginate_backwards"),
+    //         "pubdate": this.get("pubdate"),
+    //         "publisher": this.get("publisher"),
+    //         "rights": this.get("rights"),
+    //         "spread": this.get("spread"),
+    //         "src_url": this.get("src_url"),
+    //         "title": this.get("title")
+    //     };
+    // },
 
 	// ------------------------------------------------------------------------------------ //
 	//  "PRIVATE" HELPERS                                                                   //
@@ -93,7 +123,6 @@ Epub.EPUBController = Backbone.Model.extend({
 	resolvePath: function(path) {
 		return this.packageDocument.resolvePath(path);
 	},
-
 
 	// REFACTORING CANDIDATE: I think this should get moved up to the epub controller
     resolveUri : function(rel_uri) {
