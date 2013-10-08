873b8b37a48919a7f4f0763648928b2e61c94df6
Dmitry M <dmitrym@evidentpoint.com>
2013-10-08 09:42:24 -0700

    Add logging to the test server.

--- samples-project-testing/test_site_server.js
+++ samples-project-testing/test_site_server.js
@@ -1,5 +1,10 @@
 //This server is here, because we need to go up a directory from the test site for some resources.
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(3000);
+var connect = require('connect'), http = require('http');
+
+var app = connect()
+    .use(connect.logger('short'))
+    .use(connect.static(__dirname));
+
+
+    
+http.createServer(app).listen(3000);
