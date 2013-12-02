b40dfeb75263994045f8336c3dad8ebdaeb1d04d
Dmitry M <dmitrym@evidentpoint.com>
2013-12-02 13:28:32 -0800

    Return an epub3 toc list directly usable by a reader

--- epub-modules/epub/src/models/package_document.js
+++ epub-modules/epub/src/models/package_document.js
@@ -342,6 +342,14 @@ define(['require', 'module', 'jquery', 'underscore', 'backbone', 'URIjs', './man
             });
         }
 
+        // 
+        this.getEpub3Toc = function(callback) {
+            this.generateTocListDOM(function(dom) {
+                var olTocList =  $($('nav#toc', dom)[0]).children()[0];
+                callback(olTocList);
+            });
+        };
+
         function tocIsNcx() {
 
             var tocItem = getTocItem();
