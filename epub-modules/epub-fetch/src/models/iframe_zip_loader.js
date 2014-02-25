2699d3381b1a520a3f298d6596d29234e23d95b2
Aleksander Adamowski <aleksander.adamowski@gmail.com>
2014-02-25 23:15:45 +0100

    Extract deobfuscation/decryption logic to a new encryption_handler submodule.
    Small naming fixes in publication_fetcher.

--- epub-modules/epub-fetch/src/models/iframe_zip_loader.js
+++ epub-modules/epub-fetch/src/models/iframe_zip_loader.js
@@ -10,16 +10,19 @@ define([], function(){
 
         this.loadIframe = function(iframe, src, callback, caller, attachedData) {
 
-            if (getCurrentResourceFetcher().shouldFetchProgrammatically()) {
-                basicIframeLoader.loadIframe(iframe, src, callback, caller, attachedData);
-            } else {
-                var basicLoadCallback = function(success) {
-                    getCurrentResourceFetcher().fetchContentDocument(attachedData, function(resolvedContentDocumentDom){
-                        var contentDocument = iframe.contentDocument;
-                        contentDocument.replaceChild(resolvedContentDocumentDom.documentElement,
-                            contentDocument.documentElement);
-                        callback.call(caller, success, attachedData);
-                    });
+            var shouldFetchProgrammatically = getCurrentResourceFetcher().shouldFetchProgrammatically();
+            if (shouldFetchProgrammatically) {
+                var basicLoadCallback = function (success) {
+                    getCurrentResourceFetcher().fetchContentDocument(attachedData,
+                        function (resolvedContentDocumentDom) {
+                            var contentDocument = iframe.contentDocument;
+                            contentDocument.replaceChild(resolvedContentDocumentDom.documentElement,
+                                contentDocument.documentElement);
+                            callback.call(caller, success, attachedData);
+                        }, function (err) {
+                            callback.call(caller, success, attachedData);
+                        }
+                    );
                 };
                 // Feed an artificial empty HTML document to the IFRAME, then let the wrapper onload function
                 // take care of actual document loading (from zipped EPUB) and calling callbacks:
@@ -28,6 +31,8 @@ define([], function(){
                 );
 
                 basicIframeLoader.loadIframe(iframe, emptyDocumentDataUri, basicLoadCallback, caller, attachedData);
+            } else {
+                basicIframeLoader.loadIframe(iframe, src, callback, caller, attachedData);
             }
         };
     };
--- epub-modules/epub-fetch/src/models/publication_fetcher.js
+++ epub-modules/epub-fetch/src/models/publication_fetcher.js
@@ -1,37 +1,34 @@
 define(['require', 'module', 'jquery', 'URIjs', './markup_parser', './plain_resource_fetcher', './zip_resource_fetcher',
-    './content_document_fetcher'],
-    function (require, module, $, URI, MarkupParser, PlainResourceFetcher, ZipResourceFetcher, ContentDocumentFetcher) {
+    './content_document_fetcher', './encryption_handler'],
+    function (require, module, $, URI, MarkupParser, PlainResourceFetcher, ZipResourceFetcher, ContentDocumentFetcher,
+              EncryptionHandler) {
 
-    var ResourceFetcher = function(rootUrl, libDir) {
+    var PublicationFetcher = function(rootUrl, libDir) {
 
         var self = this;
 
-        ResourceFetcher.contentTypePackageReadStrategyMap = {
+        self.contentTypePackageReadStrategyMap = {
             'application/oebps-package+xml': 'exploded',
             'application/epub+zip': 'zipped',
             'application/zip': 'zipped'
         };
 
-        var ENCRYPTION_METHODS = {
-            'http://www.idpf.org/2008/embedding': embeddedFontDeobfuscateIdpf,
-            'http://ns.adobe.com/pdf/enc#RC': embeddedFontDeobfuscateAdobe
-        };
-        var _isExploded;
+        var _shouldFetchProgrammatically;
         var _dataFetcher;
+        var _encryptionHandler;
         var _packageFullPath;
         var _packageDom;
         var _packageDomInitializationDeferred;
-        var _encryptionDom;
-        var _encryptionHash;
-        var _packageJson;
 
         this.markupParser = new MarkupParser();
 
         this.initialize =  function(callback) {
 
-            _isExploded = isExploded();
+            var isEpubExploded = isExploded();
 
-            createDataFetcher(_isExploded, callback);
+            // Non exploded EPUBs (i.e. zipped .epub documents) should be fetched in a programmatical manner:
+            _shouldFetchProgrammatically = !isEpubExploded;
+            createDataFetcher(isEpubExploded, callback);
         };
 
 
@@ -71,69 +68,6 @@ define(['require', 'module', 'jquery', 'URIjs', './markup_parser', './plain_reso
             }
         }
 
-        function blob2BinArray(blob, callback) {
-            var fileReader = new FileReader();
-            fileReader.onload = function(){
-                var arrayBuffer = this.result;
-                callback(new Uint8Array(arrayBuffer));
-            };
-            fileReader.readAsArrayBuffer(blob);
-        }
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        function xorObfuscatedBlob(obfuscatedResourceBlob, prefixLength, xorKey, callback) {
-            var obfuscatedPrefixBlob = obfuscatedResourceBlob.slice(0, prefixLength);
-            blob2BinArray(obfuscatedPrefixBlob, function (bytes) {
-                var masklen = xorKey.length;
-                for (var i = 0; i < prefixLength; i++) {
-                    bytes[i] = bytes[i] ^ (xorKey[i % masklen]);
-                }
-                var deobfuscatedPrefixBlob = new Blob([bytes], { type: obfuscatedResourceBlob.type });
-                var remainderBlob = obfuscatedResourceBlob.slice(prefixLength);
-                var deobfuscatedBlob = new Blob([deobfuscatedPrefixBlob, remainderBlob],
-                    { type: obfuscatedResourceBlob.type });
-
-                callback(deobfuscatedBlob);
-            });
-        }
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        function embeddedFontDeobfuscateIdpf(obfuscatedResourceBlob, callback) {
-            var uid = _packageJson.metadata.id;
-            var hashedUid = window.Crypto.SHA1(unescape(encodeURIComponent(uid.trim())), { asBytes: true });
-            var prefixLength = 1040;
-            // Shamelessly copied from
-            // https://github.com/readium/readium-chrome-extension/blob/26d4b0cafd254cfa93bf7f6225887b83052642e0/scripts/models/path_resolver.js#L102 :
-            xorObfuscatedBlob(obfuscatedResourceBlob, prefixLength, hashedUid, callback);
-        }
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        function urnUuidToByteArray(id) {
-            var uuidRegexp = /(urn:uuid:)?([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12})/i;
-            var matchResults = uuidRegexp.exec(id);
-            var rawUuid =  matchResults[2]+matchResults[3]+matchResults[4]+matchResults[5]+matchResults[6];
-            if (! rawUuid || rawUuid.length != 32) {
-                console.error('Bad UUID format for ID :' + id);
-            }
-            var byteArray = [];
-            for (var i = 0; i < 16; i++) {
-                var byteHex =  rawUuid.substr(i*2, 2);
-                var byteNumber = parseInt(byteHex, 16);
-                byteArray.push(byteNumber);
-            }
-            return byteArray;
-        }
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        function embeddedFontDeobfuscateAdobe(obfuscatedResourceBlob, callback) {
-            var uid = _packageJson.metadata.id;
-            // extract the UUID and convert to big-endian binary form (16 bytes):
-            var uidWordArray = urnUuidToByteArray(uid);
-            var prefixLength = 1024;
-            xorObfuscatedBlob(obfuscatedResourceBlob, prefixLength, uidWordArray, callback)
-        }
-
-
         // PUBLIC API
 
         /**
@@ -146,19 +80,19 @@ define(['require', 'module', 'jquery', 'URIjs', './markup_parser', './plain_reso
          * false if documents can be fed directly into a window or iframe by src URL without using special fetching logic.
          */
         this.shouldFetchProgrammatically = function (){
-            return _isExploded;
+            return _shouldFetchProgrammatically;
         };
 
         this.getPackageUrl = function() {
             return _dataFetcher.getPackageUrl();
         };
 
-        this.fetchContentDocument = function (attachedData, contentDocumentResolvedCallback) {
+        this.fetchContentDocument = function (attachedData, contentDocumentResolvedCallback, errorCallback) {
 
             var contentDocumentFetcher = new ContentDocumentFetcher(attachedData.spineItem, self);
             contentDocumentFetcher.fetchContentDocumentAndResolveDom(contentDocumentResolvedCallback, function (err) {
                 _handleError(err);
-                callback.call(caller, success, attachedData);
+                errorCallback(err);
             });
         };
 
@@ -189,69 +123,6 @@ define(['require', 'module', 'jquery', 'URIjs', './markup_parser', './plain_reso
             var rootFile = $('rootfile', containerXmlDom);
             var packageFullPath = rootFile.attr('full-path');
             return packageFullPath;
-        };
-
-        this.getEncryptionDom = function (callback, onerror) {
-            if (_encryptionDom) {
-                callback(_encryptionDom);
-            } else {
-                self.getXmlFileDom('META-INF/encryption.xml', function (encryptionDom) {
-                    _encryptionDom = encryptionDom;
-                    callback(_encryptionDom);
-                }, onerror);
-            }
-        };
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        this._initializeEncryptionHash = function (encryptionInitializedCallback) {
-            self.getEncryptionDom(function (encryptionDom) {
-                if (!_encryptionHash) {
-                    _encryptionHash = {};
-                }
-
-                var isEncryptionSpecified = false;
-
-                var encryptedData = $('EncryptedData', encryptionDom);
-                encryptedData.each(function (index, encryptedData) {
-                    var encryptionAlgorithm = $('EncryptionMethod', encryptedData).first().attr('Algorithm');
-
-                    // For some reason, jQuery selector "" against XML DOM sometimes doesn't match properly
-                    var cipherReference = $('CipherReference', encryptedData);
-                    cipherReference.each(function (index, CipherReference) {
-                        var cipherReferenceURI = $(CipherReference).attr('URI');
-                        console.log('Encryption/obfuscation algorithm ' + encryptionAlgorithm + ' specified for ' +
-                            cipherReferenceURI);
-                        isEncryptionSpecified = true;
-                        _encryptionHash[cipherReferenceURI] = encryptionAlgorithm;
-                    });
-                });
-                if (_isExploded && isEncryptionSpecified) {
-                    _isExploded = false;
-                }
-                encryptionInitializedCallback();
-            }, function (error) {
-                console.log(error.message);
-                console.log("Document doesn't make use of encryption.");
-                encryptionInitializedCallback();
-            });
-        };
-
-        // TODO: move out to the epub module, as a new "encryption" submodule?
-        this.getEncryptionMethodForRelativePath = function(pathRelativeToRoot) {
-            if (_encryptionHash){
-                return _encryptionHash[pathRelativeToRoot];
-            }   else {
-                return undefined;
-            }
-        };
-
-        this.getDecryptionFunctionForRelativePath = function (pathRelativeToRoot) {
-            var encryptionMethod = self.getEncryptionMethodForRelativePath(pathRelativeToRoot);
-            if (ENCRYPTION_METHODS[encryptionMethod]) {
-                return ENCRYPTION_METHODS[encryptionMethod];
-            } else {
-                return undefined;
-            }
         };
 
         this.getPackageDom = function (callback, onerror) {
@@ -309,12 +180,24 @@ define(['require', 'module', 'jquery', 'URIjs', './markup_parser', './plain_reso
 
         // Currently needed for deobfuscating fonts
         this.setPackageJson = function(packageJson, settingFinishedCallback) {
-            _packageJson = packageJson;
-            this._initializeEncryptionHash(settingFinishedCallback);
+
+            _encryptionHandler = new EncryptionHandler(packageJson, self);
+
+            _encryptionHandler.initializeEncryptionHash(function() {
+                if (_encryptionHandler.isEncryptionSpecified()) {
+                    // EPUBs that use encryption for any resources should be fetched in a programmatical manner:
+                    _shouldFetchProgrammatically = true;
+                }
+                settingFinishedCallback();
+            });
         };
+
+        this.getDecryptionFunctionForRelativePath = function(pathRelativeToRoot) {
+            return _encryptionHandler.getDecryptionFunctionForRelativePath(pathRelativeToRoot);
+        }
 
     };
 
-    return ResourceFetcher
+    return PublicationFetcher
 
 });
