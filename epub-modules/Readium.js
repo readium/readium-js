3816b35ef5e9f4bfb57ade929c65496e998e4a10
Boris Schneiderman <boriss@evidentpoint.com>
2014-06-18 14:19:22 -0700

    exposed reader as ReadiumSDK.reader object for autotest system being able to call reader's APIs

--- epub-modules/Readium.js
+++ epub-modules/Readium.js
@@ -76,6 +76,9 @@ define(['require', 'module', 'console_shim', 'jquery', 'underscore', 'readerView
             });
         }
 
+        //we need global access to the reader object for automation test being able to call it's APIs
+        ReadiumSDK.reader = this.reader;
+
         ReadiumSDK.trigger(ReadiumSDK.Events.READER_INITIALIZED, this.reader);
     };
 
