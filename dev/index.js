b36cd8ed22c80dafeaf5183761df179b89d4e21c
danielweck <daniel.weck@gmail.com>
2015-12-08 15:46:26 +0000

    submod (support for highlights plugin)

--- dev/index.js
+++ dev/index.js
@@ -139,17 +139,21 @@ require(["readium_shared_js/globalsSetup", "readium_shared_js/globals"], functio
 
                 Globals.logEvent("PLUGINS_LOADED", "ON", "dev/index.js");
                 
-                // readium built-in (should have been require()'d outside this scope)
+                // legacy (should be undefined / null)
                 console.log(reader.plugins.annotations);
-                if (reader.plugins.annotations) {
-                    reader.plugins.annotations.initialize({annotationCSSUrl: readerOptions.annotationCSSUrl});
-                    reader.plugins.annotations.on("annotationClicked", function(type, idref, cfi, id) {
+                
+                // same as above, new implementation
+                console.log(reader.plugins.highlights);
+                
+                if (reader.plugins.highlights) {
+                    reader.plugins.highlights.initialize({annotationCSSUrl: readerOptions.annotationCSSUrl});
+                    reader.plugins.highlights.on("annotationClicked", function(type, idref, cfi, id) {
                         console.log("ANNOTATION CLICK: " + id);
-                        reader.plugins.annotations.removeHighlight(id);
+                        reader.plugins.highlights.removeHighlight(id);
                     });
-                    reader.plugins.annotations.on("textSelectionEvent", function() {
+                    reader.plugins.highlights.on("textSelectionEvent", function() {
                         console.log("ANNOTATION SELECT");
-                        reader.plugins.annotations.addSelectionHighlight(Math.floor((Math.random()*1000000)), "highlight");
+                        reader.plugins.highlights.addSelectionHighlight(Math.floor((Math.random()*1000000)), "highlight");
                     });
                 }
 
